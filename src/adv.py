8113d47a64463a21d38698a583c228ddfdd26881
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-03 17:58:12 -0800

    solutions

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,8 @@
 from room import Room
+from player import Player 
 
 # Declare all the rooms
+
 
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -35,15 +37,49 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
+name = input('Welcome to the game! What is your name? ')
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(name, room['outside'])
+
+direction = None
+current_room = None
+input('Press any key ')
+
+#This is how we are reassigning
+
+while direction != "q":
+    if current_room != player.current_room:
+        current_room = player.current_room
+    print(f'{player.current_room.name }-- \n')    
+    print(f"{player.current_room.description} \n")
+    direction = input('Choose your direction. Type: n,s,e, or w ')
+    if direction.lower() in ["n","s","e","w"]:
+        if direction.lower() == "n":
+            current_room = current_room.n_to
+           
+        elif direction.lower() == "s":
+            current_room = current_room.s_to
+           
+        elif direction.lower() == "e":
+            current_room = current_room.e_to
+            
+        elif direction.lower() == "w":
+            current_room = current_room.w_to
+            
+    if current_room is None:
+        print('Oh Noes, You can\'t go there! That is a wall. \n')
+    else:
+        player.current_room = current_room
+
 # Write a loop that:
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
+##* Prints the current room name
+## * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
+##Go play a mud, 
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+  ##RAINBOW BRACERS 
+
+#ROOM MADE YAY!
+class Room:
+  def __init__ (self, name, description):
+    self.name = name
+    self.description = description
+    
+    self.s_to = None
+    self.n_to = None
+    self.e_to = None
+    self.w_to = None
+ 
+  def __str__ (self):
+    return f"{self.description}"
